cbbf2ac26638b3ed1dfdd6cfec04f463657c7436
Eduardo Antonio Campos Martinez <eduardo.camposmartinez@lifemiles.com>
2024-11-17 22:24:23 -0600

    Refactor: Add support for multiple documents

--- internal/platform/rest/entities.go
+++ internal/platform/rest/entities.go
@@ -1,6 +1,10 @@
 package rest
 
 type Request struct {
+	Documents []Document `json:"documents"`
+}
+
+type Document struct {
 	Type         string `json:"type"`
 	Value        string `json:"value"`
 	IssueCountry string `json:"issueCountry"`
